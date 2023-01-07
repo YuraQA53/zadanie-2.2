d6de070222b0a99cdd95386af456492991977669
Yuriy <yura.qa53@gmail.com>
2023-01-07 22:37:35 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int valueBill = 300; // начальный баланс
+        int refill = 1100; // пополнение счета
+        int condition = 1000; // условия выполнения
+        int bonus = 0;
+        if (refill > condition) {
+            bonus = refill / 100;
+        }
+        System.out.println("Итоговый счет:");
+        System.out.println(valueBill + refill);
+        System.out.println("Количество бонусных рублей:");
+        System.out.println(bonus);
+    }
+
+
+    }
